b28156b1c91f053c0611d2ffa0f90c3c258765af
Igor Shklyar <igor.shklyar@alfabank.kiev.ua>
2017-05-30 13:10:08 +0300

    Added getValidator method to config. Messages are properly shown now. Source: https://stackoverflow.com/questions/6884795/cant-get-hibernate-validator-working-with-spring-messagesource

--- src/main/java/com/course/config/AppConfig.java
+++ src/main/java/com/course/config/AppConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -47,12 +47,16 @@ public class AppConfig extends WebMvcConfigurerAdapter{
         return resolver;
     }
 
-    //
     @Bean(name = "validator")
     public LocalValidatorFactoryBean validator(){
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
     }
 
     @Override
