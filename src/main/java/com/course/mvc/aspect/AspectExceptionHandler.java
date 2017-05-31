9cd0b3385e67723692a75e4bf43c2d9edd0b793a
Igor Shklyar <igor.shklyar@alfabank.kiev.ua>
2017-05-31 11:36:37 +0300

    ExceptionHandler was corrected Handler methods which are annotated with this annotation are  not allowed to have RedirectAttributes signature.
    https://stackoverflow.com/questions/14961869/performing-a-redirect-from-a-spring-mvc-exceptionhandler-method/

--- src/main/java/com/course/mvc/aspect/AspectExceptionHandler.java
+++ src/main/java/com/course/mvc/aspect/AspectExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.course.mvc.aspect;
 
-import com.course.mvc.dto.ChatUserDto;
 import com.course.mvc.exceptions.ServiceException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Alexey on 27.05.2017.
@@ -12,11 +16,42 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class AspectExceptionHandler {
 
-    @ExceptionHandler(value = ServiceException.class)
-    public String saveUserError(ServiceException ex, RedirectAttributes attributes){
-        attributes.addFlashAttribute("error",ex.getMessage());
-        attributes.addFlashAttribute("save",new ChatUserDto());
-        return "redirect:/registration";
+    /**
+     * Не используем редирект
+     *
+     * @param exception
+     * @return
+     */
+//    @ExceptionHandler(ServiceException.class)
+//    public ModelAndView handleServiceException(Exception exception) {
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("error", exception.getMessage());
+//        mav.addObject("user", new ChatUserDto());
+//        mav.setViewName("registration");
+//        return mav;
+//    }
+
+    /**
+     * For each Spring controller we can simply define a method that automatically gets called if a given exception occurs.
+     * Thus whenever an ServiceException is raised from any controller method will call the above method handleServiceException().
+     * We mapped ServiceException.class to this method using @ExceptionHandler annotation.
+     *
+     * Используем редирект
+     *
+     * @param exception
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(ServiceException.class)
+    public RedirectView handleServiceException(Exception exception, HttpServletRequest request) {
+        RedirectView rw = new RedirectView("./registration");
+        rw.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+
+        FlashMap outputFlashMap = RequestContextUtils.getOutputFlashMap(request);
+        if (outputFlashMap != null){
+            outputFlashMap.put("error", exception.getMessage());
+        }
+        return rw;
     }
 
 }
